dddec684d9cc3a56116a68d2bfccf53c1a0b5bbb
tai.nguyen <tai.nguyen24816@gmail.com>
2025-01-09 22:20:30 +0700

    update source print CAN mode support

--- src/main.c
+++ src/main.c
@@ -1,19 +1,33 @@
 #include <stdint.h>
-#include <zephyr/drivers/can/can_mcan.h>
+#include <zephyr/drivers/can.h>
 
 const struct device *device;
+struct can_filter can_filter_user =
+{
+    .id = 0xff,
+    .mask = 0xff,
+    .flags = CAN_FILTER_IDE
+};
 
-// #if DT_NODE_HAS_STATUS(DT_NODELABEL(can1), okay)
-// #error "Hello _11111"
-// #else
-// #error "Node is disabled"
-// #endif
+static void can_recv_call_back(const struct device *dev, struct can_frame *frame, void *user_data)
+{
+    return;
+}
 
 int main(void)
 {
+    int ret = 0;
+    can_mode_t can_mode_support;
+
     device = DEVICE_DT_GET_ONE(st_stm32_bxcan);
 
+    ret = can_get_capabilities(device, &can_mode_support);
+    if(0 == ret)
+    {
+        printf("can_mode_support = %d\n", can_mode_support);
+    }
 
+    ret = can_add_rx_filter(device, can_recv_call_back, NULL, &can_filter_user);
 
     return 0;
 }
