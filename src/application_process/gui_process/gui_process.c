6734ca314a64d1297e9acc054ff3971acc204b35
tai.nguyen <tai.nguyen24816@gmail.com>
2025-02-04 22:36:05 +0700

    update gui process, need reconfig lv_conf.h to enable music test

---
+++ src/application_process/gui_process/gui_process.c
@@ -0,0 +1,39 @@
+#include <stdint.h>
+#include <zephyr/kernel.h>
+#include "gui_process.h"
+#include "gui.h"
+
+#define GUI_THREAD_STACK    2048
+#define GUI_THREAD_PRIORITY 8
+
+extern void gui_thread(void *, void *, void *);
+
+K_THREAD_STACK_DEFINE(gui_thread_stack_size, GUI_THREAD_PRIORITY);
+static struct k_thread gui_thread_data;
+
+static k_tid_t thread_id;
+
+
+void gui_process_init(void)
+{
+    /* init gui */
+    gui_init();
+
+    /* init gui thread */
+    thread_id = k_thread_create(&gui_thread_data, gui_thread_stack_size,
+                                K_THREAD_STACK_SIZEOF(gui_thread_stack_size),
+                                gui_thread,
+                                NULL, NULL, NULL,
+                                GUI_THREAD_PRIORITY, 0, K_FOREVER);
+}
+
+
+void gui_thread(void * param1, void * param2, void * param3)
+{
+    for(;;)
+    {
+        printf("gui thread is running\n");
+        gui_handler();
+    }
+}
+
---
+++ src/low_level/gui/gui.c
@@ -0,0 +1,48 @@
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/display.h>
+#include <lvgl.h>
+#include <lvgl_mem.h>
+#include <lv_demos.h>
+#include <stdio.h>
+
+#define CONFIG_LV_USE_DEMO_MUSIC 1
+
+#include <lv_demo_music.h>
+
+void gui_handler(void)
+{
+    uint32_t sleep_ms = lv_task_handler();
+
+	k_msleep(MIN(sleep_ms, INT32_MAX));
+}
+
+void gui_init(void)
+{
+    const struct device *display_dev;
+
+	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(display_dev)) {
+		printf("Device not ready, aborting test");
+	}
+
+#if defined(CONFIG_LV_USE_DEMO_MUSIC)
+	lv_demo_music();
+#elif defined(CONFIG_LV_USE_DEMO_BENCHMARK)
+	lv_demo_benchmark();
+#elif defined(CONFIG_LV_USE_DEMO_STRESS)
+	lv_demo_stress();
+#elif defined(CONFIG_LV_USE_DEMO_WIDGETS)
+	lv_demo_widgets();
+#else
+#error Enable one of the demos CONFIG_LV_USE_DEMO_MUSIC, CONFIG_LV_USE_DEMO_BENCHMARK ,\
+	CONFIG_LV_USE_DEMO_STRESS, or CONFIG_LV_USE_DEMO_WIDGETS
+#endif
+
+	lv_task_handler();
+	display_blanking_off(display_dev);
+
+}
+
+
+
