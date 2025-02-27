0a4fb7df20e5667002d943dd8a5522449d6c8e48
tai.nguyen <tai.nguyen24816@gmail.com>
2025-02-27 22:35:08 +0700

    update git ignore

--- src/low_level/gui/gui_design/lvgl-simulator/build/object/lv_utils.d
+++
@@ -1,5 +0,0 @@
-build/object/lv_utils.o: \
- d:\zephyr\zephyrproject\zephyr\app\can_application\src\low_level\gui\gui_design\lvgl\src\misc\lv_utils.c \
- d:\zephyr\zephyrproject\zephyr\app\can_application\src\low_level\gui\gui_design\lvgl\src\misc\lv_utils.h
-
-d:\zephyr\zephyrproject\zephyr\app\can_application\src\low_level\gui\gui_design\lvgl\src\misc\lv_utils.h:
